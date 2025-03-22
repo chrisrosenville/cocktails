38c2cc1043e0b5d569d2e4603f39ca09d81951a7
Christian Rosenville <web.chrisr@gmail.com>
2025-03-22 11:53:51 +0100

    wowsa

--- app/drink/[id]/page.tsx
+++ app/drink/[id]/page.tsx
@@ -7,6 +7,17 @@ import { extractIngredients } from "@/utils/extractIngredients";
 import { useParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 
+interface TagProps {
+  name: string;
+}
+function TagContainer(props: TagProps) {
+  return (
+    <span className="bg-neutral-400 text-center rounded-md py-1 text-xs px-2">
+      {props.name}
+    </span>
+  );
+}
+
 export default function DrinkPage() {
   const { id } = useParams();
 
@@ -25,32 +36,35 @@ export default function DrinkPage() {
 
   return (
     <main className="min-h-svh md:min-h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-2 grid-flow-row p-4 ">
-        <div className="relative w-full h-[80vw] md:h-[50vw] max-h-[600px] overflow-hidden rounded-xl">
-          <Image
-            src={data?.drinks[0]?.strDrinkThumb ?? ""}
-            alt={data?.drinks[0]?.strDrink ?? "Cocktail"}
-            fill
-            sizes="300px 300px, (min-width: 640px) 600px 600px"
-          />
+      <div className="relative aspect-square w-full md:h-[50vw] max-h-[600px] overflow-hidden">
+        <Image
+          src={data?.drinks[0]?.strDrinkThumb ?? ""}
+          alt={data?.drinks[0]?.strDrink ?? "Cocktail"}
+          fill
+          sizes="300px 300px, (min-width: 640px) 600px 600px"
+        />
+      </div>
+
+      <div className="p-4">
+        <h1 className="text-4xl font-bold font-OpenSans mb-4">
+          {data?.drinks[0]?.strDrink}
+        </h1>
+
+        <span className="">Tags:</span>
+
+        <div className="flex flex-wrap text-sm gap-2 py-1">
+          <TagContainer name={data?.drinks[0]?.strCategory} />
+          <TagContainer name={data?.drinks[0]?.strAlcoholic} />
+          <TagContainer name={data?.drinks[0]?.strGlass} />
         </div>
-        <div className="mt-2 md:mt-0 md:ml-4">
-          <h1 className="text-4xl font-bold font-Oswald">
-            {data?.drinks[0]?.strDrink}
-          </h1>
-          <div className="flex flex-col py-4 text-xs">
-            <span>{data?.drinks[0]?.strCategory}</span>
-            <span>{data?.drinks[0]?.strAlcoholic}</span>
-            <span>{data?.drinks[0]?.strGlass}</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="underline underline-offset-2">Ingredients</span>
-            <p>{`${ingredients}`}</p>
-          </div>
-          <div className="mt-4">
-            <span className="underline underline-offset-2">Instructions</span>
-            <p>{data?.drinks[0]?.strInstructions}</p>
-          </div>
+
+        <div className="flex flex-col mt-4">
+          <span className="underline underline-offset-2">Ingredients</span>
+          <p>{`${ingredients}`}</p>
+        </div>
+        <div className="mt-4">
+          <span className="underline underline-offset-2">Instructions</span>
+          <p>{data?.drinks[0]?.strInstructions}</p>
         </div>
       </div>
     </main>
